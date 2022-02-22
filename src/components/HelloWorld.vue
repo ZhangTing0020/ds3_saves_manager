760b1c2b09db932fc8043db0b773236123ee04b5
zhangting <273664273@qq.com>
2022-02-22 21:34:23 +0800

    v1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,231 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="top">
+      <div class="left">
+        <el-button type="primary" plain class="save">主要按钮</el-button>
+      </div>
+      <div class="right">
+        <el-button type="primary" plain class="restore">主要按钮</el-button>
+      </div>
+    </div>
+
+    <div class="table">
+      <el-table
+        :data="tableData"
+        style="
+           {
+            width: 100%;
+          }
+        "
+        height="100%"
+      >
+        <el-table-column fixed prop="date" label="日期"> </el-table-column>
+        <el-table-column prop="save" label="存档"> </el-table-column>
+        <el-table-column prop="steam_id" label="steam-id"> </el-table-column>
+        <el-table-column prop="location" label="位置"> </el-table-column>
+        <el-table-column fixed="right" label="操作">
+          <template slot-scope="scope">
+            <el-button
+              @click.native.prevent="deleteRow(scope.$index, tableData)"
+              type="text"
+              size="small"
+            >
+              移除
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  methods: {
+    deleteRow(index, rows) {
+      rows.splice(index, 1);
+    },
+  },
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc",
+        },
+        {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc",
+        },
+        {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc",
+        },
+        {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc",
+        },
+        {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc",
+        },
+        {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc",
+        },
+        {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc",
+        },
+        {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc",
+        },
+        {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc",
+        },
+        {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc",
+        },
+        {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc",
+        },
+        {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc",
+        },
+        {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc",
+        },
+        {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc",
+        },
+        {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc",
+        },
+        {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc",
+        },
+        {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc",
+        },
+        {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc",
+        },
+        {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc",
+        },
+        {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc",
+        },
+        {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style scoped lang="less">
+.hello {
+  width: 100vw;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.top {
+  width: 100%;
+  height: 25vh;
+  // margin-bottom: 4vh;
+  .left {
+    display: inline-block;
+    .save {
+      width: 50vw;
+      height: 30vh;
+
+      font-size: 50px;
+    }
+  }
+  .right {
+    display: inline-block;
+    .restore {
+      width: 50vw;
+      height: 30vh;
+      font-size: 50px;
+    }
+  }
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.table {
+  height: 65vh;
+  width: 100%;
+  .el-table_2_column_6 {
+    width: 15%;
+  }
+  .el-table_2_column_7 {
+    width: 20%;
+  }
+  .el-table_2_column_8 {
+    width: 15%;
+  }
+  .el-table_2_column_9 {
+    width: 40%;
+  }
+  .el-table_2_column_10 {
+    width: 10%;
+  }
 }
 </style>
